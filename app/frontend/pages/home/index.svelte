5a44611fda3743cbfe1121658f4e4b1ddfe1cd42
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-07-19 00:15:13 -0400

    Add hub.outernet card

--- app/frontend/pages/home/index.svelte
+++ app/frontend/pages/home/index.svelte
@@ -1,1 +1,16 @@
-<h1 class="w-full text-center font-sans text-2xl">Home page</h1>
+<div class="grid grid-cols-2 gap-2 p-2">
+  <div
+    class="z-20 flex w-full flex-row rounded-md bg-white p-2 dark:bg-zinc-800"
+  >
+    <img
+      src="/icons/wave.svg"
+      alt="Wave"
+      class="h-10 w-10 md:h-12 md:w-12 lg:h-14 lg:w-14"
+    />
+    <h1
+      class="ml-2 font-inter text-4xl font-black text-amber-700 md:text-5xl lg:text-6xl"
+    >
+      hub.outernet
+    </h1>
+  </div>
+</div>
